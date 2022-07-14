df55a3ed674377879335fa361fef2dea86471617
EC2 Default User <ec2-user@ip-172-31-14-122.ap-northeast-1.compute.internal>
2022-07-14 06:33:26 +0000

    [add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,44 @@
 class BooksController < ApplicationController
-  def new
+  def index
+    @book=Book.new
+    @books=Book.all
   end
 
-  def index
+  def create
+    @book=Book.new(book_params)
+    @book.save
+     if @book.save
+      redirect_to book_path(@book), notice: 'Book was successfully created.'
+     else
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
+     end
   end
 
   def show
+    @book=Book.find(params[:id])
   end
 
   def edit
+    @book=Book.find(params[:id])
   end
+
+  def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to book_path(book.id)
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
 end
+
